f23a2bd479a54e10a8356fefd8f9b344c4cffcff
Ismail Ali <ishaliprime@gmail.com>
2023-05-04 18:32:43 -0500

    completed part one and got the colored blocks to appear when i click the corresponding buttons

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log('🟥 🟦 🟩 🟨');
+$(document).ready(onReady);
+
+let counter = 0
+
+function onReady(){
+    // handle button clicks
+    $('#red-btn').on('click', appendRed);
+    $('#blue-btn').on('click', appendBlue)
+    $('#green-btn').on('click', appendGreen)
+    $('#yellow-btn').on('click', appendYellow)
+    // $('#blocks').on('click', '.block', removeBlock)
+    
+}
+
+let red = 1;
+
+function appendRed(){
+    $('#blocks').append('<div class="block red-fill"></div>')
+}
+function appendBlue(){
+    $('#blocks').append('<div class="block blue-fill"></div>')
+}
+function appendGreen(){
+    $('#blocks').append('<div class="block green-fill"></div>')
+}
+function appendYellow(){
+    $('#blocks').append('<div class="block yellow-fill"></div>')
+}
+// function removeBlock(){
+//     $(this).remove()
+
+
+// }
