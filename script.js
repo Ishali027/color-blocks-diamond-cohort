77c40f04937bcd4fe5d2c30c9ae2c2e4cf2ddcbc
Ismail Ali <ishaliprime@gmail.com>
2023-05-04 21:29:49 -0500

    wrote the code that counts the number of blocks every time you click a button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log('🟥 🟦 🟩 🟨');
 $(document).ready(onReady);
 
-let counter = 0
+
 
 function onReady(){
     // handle button clicks
@@ -14,18 +14,35 @@ function onReady(){
 }
 
 let red = 1;
+let blue = 1;
+let green = 1;
+let yellow = 1;
 
 function appendRed(){
     $('#blocks').append('<div class="block red-fill"></div>')
+    $('#red-count').empty()
+    red++;
+    $('#red-count').append(red)
+
 }
+
 function appendBlue(){
     $('#blocks').append('<div class="block blue-fill"></div>')
+    $('#blue-count').empty()
+    blue++;
+    $('#blue-count').append(blue)
 }
 function appendGreen(){
     $('#blocks').append('<div class="block green-fill"></div>')
+    $('#green-count').empty()
+    green++;
+    $('#green-count').append(green)
 }
 function appendYellow(){
-    $('#blocks').append('<div class="block yellow-fill"></div>')
+$('#blocks').append('<div class="block yellow-fill"></div>')
+$('#yellow-count').empty()
+yellow++;
+$('#yellow-count').append(yellow)
 }
 // function removeBlock(){
 //     $(this).remove()
